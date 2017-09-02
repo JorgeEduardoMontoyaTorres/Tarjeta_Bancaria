96eac24cde766f7f7a5bbc32b1e79ab3c4405c9e
Jorge Eduardo Montoya Torres <lalo_realmadrid93@hotmail.com>
2017-09-01 19:51:34 -0500

    Se documento el codigo.

--- Tarjeta.java
+++ Tarjeta.java
@@ -1,3 +1,7 @@
+/**
+ * Clase tarjeta donde se puede depositar, retirar y consultar saldo de una tarjeta
+ * @version 21 agosto 2017
+ */
 public class Tarjeta
 {
     private double saldo;
@@ -7,11 +11,20 @@ public class Tarjeta
         saldo = 0;
     }
     
+    /**
+     * Deposita el dinero a la tarjeta.
+     * @param dinero Cantidad a abonar a la tarjeta.
+     */
     public void depositar(double dinero)
     {
         saldo = saldo + dinero;
     }
     
+    /**
+     * Retira una determinada cantidad de la tarjeta.
+     * @param dinero Cantidad deseada a retirar.
+     * @return Regresa 0 en caso de que ya no haya dinero en la tarjeta.
+     */
     public double retirar(double dinero)
     {
         if (saldo >= dinero)
@@ -23,6 +36,9 @@ public class Tarjeta
             return 0; 
     }
     
+    /**
+     * Consulta el saldo de la tarjeta y regresa el monto existente.
+     */
     public void consultar()
     {
         System.out.println("Saldo actual: " +saldo);
